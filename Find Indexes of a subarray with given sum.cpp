efde8ff561e82b3997dd979b53e054ff3f45a255
UalexU <alexnvillalba@gmail.com>
2024-03-11 23:20:16 +0100

    Added gitignore and a problem

---
+++ Find Indexes of a subarray with given sum.cpp
@@ -0,0 +1,9 @@
+class Solution
+{
+    public:
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector<int> subarraySum(vector<int>arr, int n, long long s)
+    {
+        // Your code here
+    }
+};
